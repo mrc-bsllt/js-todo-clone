c70ff689e346770276817fe3567542004b4ae404
MarcoB <mrc.bsllt@gmail.com>
2020-11-30 14:45:54 +0100

    impostata la to do list

--- js/script.js
+++ js/script.js
@@ -1,21 +1,55 @@
 $(document).ready(
   function() {
 
+    // HEADER-----------------------------------------------
     var userName = prompt("Inserisci il tuo NOME!");
     userName = userName.toUpperCase();
 
-    var source = $("#entry-template").html();
-    var template = Handlebars.compile(source);
+    var sourceHeader = $("#entry-template").html();
+    var templateHeader = Handlebars.compile(sourceHeader);
 
-    var context = {
+    var contextHeader = {
       title: "Ciao " +  userName + "!",
       body: "Questo è il mio esempio di post creato con un template Handlebars!"
     };
 
-    var html = template(context);
-    console.log(html);
+    var html = templateHeader(contextHeader);
+    // console.log(html);
 
     $("header").append(html);
+    // HEADER-----------------------------------------------
+
+    // MAIN-------------------------------------------------
+    var toDoList = [
+      {
+        todoContent: "Fare la spesa"
+      },
+      {
+        todoContent: "Pagare le bollette"
+      },
+      {
+        todoContent: "Pagare l'affitto"
+      },
+      {
+        todoContent: "Lavarmi"
+      },
+      {
+        todoContent: "Preparare la cena"
+      },
+    ];
+
+    var sourceMain = $("#todo_template").html();
+    var templateMain = Handlebars.compile(sourceMain);
+
+    for (var i = 0; i < toDoList.length; i++) {
+
+      var contextMain = toDoList[i];
+      var html = templateMain(toDoList[i]);
+      $("main #todo_list").append(html);
+
+    }
+    // MAIN-------------------------------------------------
+
 
   }
 );
