45000499d80fd0e129ef9b6db4598d01a6d1beaa
MarcoB <mrc.bsllt@gmail.com>
2020-11-30 13:52:20 +0100

    ricopiato l'esempio del sito di Handlebars

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 $(document).ready(
   function() {
-    console.log("ciao");
+
+    var userName = prompt("Inserisci il tuo NOME!");
+    userName = userName.toUpperCase();
+
+    var source = $("#entry-template").html();
+    var template = Handlebars.compile(source);
+
+    var context = {
+      title: "Ciao " +  userName + "!",
+      body: "Questo è il mio esempio di post creato con un template Handlebars!"
+    };
+
+    var html = template(context);
+    console.log(html);
+
+    $("header").append(html);
+
   }
 );
